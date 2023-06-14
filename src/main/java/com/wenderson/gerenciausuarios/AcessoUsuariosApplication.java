fdfdaced5086d5fa4c70997e44654526b225c5b0
Wendersonfarias <wendersonpfarias@gmail.com>
2023-06-14 00:52:22 -0400

    projeto iniciado

---
+++ src/main/java/com/wenderson/gerenciausuarios/AcessoUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.wenderson.gerenciausuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wenderson/gerenciausuarios/AcessoUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenderson.gerenciausuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
